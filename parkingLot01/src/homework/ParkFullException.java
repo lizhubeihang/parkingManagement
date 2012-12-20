f0b9aed55327d35cec923d9977e9dc9b355bbbbe
lizhubeihang <lizhu5211111@126.com>
2012-12-20 11:03:50 +0800

    hehhe

--- parkingLot01/src/homework/ParkFullException.java
+++
@@ -1,9 +0,0 @@
-package homework;
-
-public class ParkFullException extends RuntimeException{
-    public ParkFullException() {
-    }
-    public ParkFullException(String message) {
-        super(message);
-    }
-}
